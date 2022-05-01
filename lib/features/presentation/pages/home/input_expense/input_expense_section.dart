956e721391cd552d36efa349819ed8756f6719a4
zackmolt <fkrihkl@gmail.com>
2022-05-02 00:29:07 +0700

    set money format

--- lib/features/presentation/pages/home/input_expense/input_expense_section.dart
+++ lib/features/presentation/pages/home/input_expense/input_expense_section.dart
@@ -1,7 +1,10 @@
+import 'package:expense_app/core/util/money_util.dart';
+import 'package:expense_app/core/util/theme_util.dart';
 import 'package:expense_app/features/domain/entities/expense_categroy.dart';
 import 'package:expense_app/features/presentation/pages/home/input_expense/category_list_widget.dart';
 import 'package:expense_app/features/presentation/widgets/floating_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class InputExpenseSection extends StatefulWidget {
   const InputExpenseSection({Key? key}) : super(key: key);
@@ -22,7 +25,7 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
 
   int _selectedItemPosition = -1;
 
-  final TextEditingController _controllerTitle = TextEditingController();
+  final TextEditingController _controllerNominal = TextEditingController();
   final TextEditingController _controllerDesc = TextEditingController();
 
   Widget _buildCategoryList(){
@@ -61,6 +64,38 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
     );
   }
 
+  void _processNominalText(String text){
+    try{
+      String nonDecimalNominal = text.replaceAll('.', '');
+      _controllerNominal.text = MoneyUtil.getReadableMoney(int.parse(nonDecimalNominal));
+      _controllerNominal.selection = TextSelection.fromPosition(TextPosition(offset: _controllerNominal.text.length));
+    }catch(e){
+
+    }
+  }
+
+  bool isInputDataValid(){
+    bool isValid = true;
+
+    if(_controllerNominal.text.isEmpty){
+      isValid = false;
+    }
+    if(_selectedItemPosition == -1){
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  void _saveExpenseData(){
+    FocusManager.instance.primaryFocus?.unfocus();
+    if(isInputDataValid()){
+      
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Some fields are required'), backgroundColor: MyTheme.red,));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     _theme = Theme.of(context);
@@ -73,10 +108,16 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
             const Text('Insert your expense'),
             const SizedBox(height: 16,),
             TextField(
-              controller: _controllerTitle,
+              controller: _controllerNominal,
               style: _theme.textTheme.bodyText1,
               keyboardType: const TextInputType.numberWithOptions(decimal: false),
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly
+              ],
               textInputAction: TextInputAction.next,
+              onChanged: (str){
+                _processNominalText(str);
+              },
               decoration: const InputDecoration(
                   labelText: 'Amount',
                   prefix: Text('Rp.'),
@@ -108,10 +149,8 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
                         )
                     ),
                   ),
-                  onPressed: (){
-
-                  },
-                  child: Text('Save',)
+                  onPressed: _saveExpenseData,
+                  child: const Text('Save',)
               ),
             )
           ],
