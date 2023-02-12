c1cf79e2f3ba97ce2f4e4232105c70a3eb5d6f09
zackmolt <fkrihkl@gmail.com>
2023-02-12 18:28:19 +0700

    feat: add text recognition handler and camera launcher

--- lib/features/presentation/pages/home/input_expense/input_expense_section.dart
+++ lib/features/presentation/pages/home/input_expense/input_expense_section.dart
@@ -1,14 +1,17 @@
 import 'package:expense_app/core/util/date_util.dart';
 import 'package:expense_app/core/util/money_util.dart';
+import 'package:expense_app/core/util/text_recognition_handler.dart';
 import 'package:expense_app/core/util/theme_util.dart';
 import 'package:expense_app/features/data/models/log_model.dart';
 import 'package:expense_app/features/domain/entities/expense_categroy.dart';
 import 'package:expense_app/features/domain/entities/expense_limit.dart';
 import 'package:expense_app/features/domain/entities/log.dart';
+import 'package:expense_app/features/injection_container.dart';
 import 'package:expense_app/features/presentation/bloc/expense_month/expense_month_bloc.dart';
 import 'package:expense_app/features/presentation/bloc/fund_source/transaction/fund_source_bloc.dart';
 import 'package:expense_app/features/presentation/bloc/balance_left/balance_left_bloc.dart';
 import 'package:expense_app/features/presentation/bloc/logs/logs_bloc.dart';
+import 'package:expense_app/features/presentation/pages/camera/camera_page.dart';
 import 'package:expense_app/features/presentation/widgets/button_widget.dart';
 import 'package:expense_app/features/presentation/widgets/floating_container.dart';
 import 'package:expense_app/features/presentation/widgets/fund_source_selectable_list.dart';
@@ -17,6 +20,8 @@ import 'package:expense_app/features/presentation/widgets/splash_effect_widget.d
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:expense_app/features/presentation/routes/route.dart' as route;
+import 'package:permission_handler/permission_handler.dart';
 
 class InputExpenseSection extends StatefulWidget {
 
@@ -34,13 +39,13 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
   late BalanceLeftBloc _balanceLeftBloc;
   late FundSourceBloc _fundSourceBloc;
   late ExpenseMonthBloc _expenseMonthBloc;
-  FundSource? _selectedFundSource;
+  late ThemeData _theme;
+  late TextRecognitionHandler _textRecognitionHandler;
 
   final ButtonWidgetController _buttonController = ButtonWidgetController();
 
-  late ThemeData _theme;
-  
   int _selectedCategoryPosition = -1;
+  FundSource? _selectedFundSource;
 
   // Edit Text Controller
   final TextEditingController _controllerNominal = TextEditingController();
@@ -57,6 +62,7 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
     _fundSourceBloc = BlocProvider.of<FundSourceBloc>(context);
     _expenseMonthBloc = BlocProvider.of<ExpenseMonthBloc>(context);
     _fundSourceBloc.add(GetFundSourceEvent());
+    _textRecognitionHandler = sl<TextRecognitionHandler>();
 
     if (widget.log != null) {
       _controllerNominal.text = MoneyUtil.getReadableMoney(widget.log!.nominal);
@@ -70,46 +76,23 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
   @override
   Widget build(BuildContext context) {
     _theme = Theme.of(context);
-    return BlocListener<LogsBloc, LogsState>(
-      listenWhen: (bloc, state) =>
-          state is InsertLogsResult ||
-          state is InsertLogsLoading ||
-          state is UpdateLogsResult ||
-          state is UpdateLogsLoading,
-      listener: (bloc, state) {
-        if (state is InsertLogsResult || state is UpdateLogsResult) {
-          // After Success Action
-          doAfterSuccess () {
-            // Then update Recent Log List data and others...
-            _logsBloc.add(GetRecentLogsEvent());
-            _balanceLeftBloc.add(GetBalanceLeftEvent());
-            _expenseMonthBloc.add(GetExpenseInMonthEvent());
-
-            // Clear Edit Text
-            _controllerNominal.text = '';
-            _controllerDesc.text = '';
-
-            _buttonController.hideLoading();
-            setState(() {});
-          }
-          if (state is InsertLogsResult && state.isSuccess) {
-            doAfterSuccess();
-          }
-          if (state is UpdateLogsResult && state.isSuccess) {
-            doAfterSuccess();
-            Navigator.pop(context);
-          }
-        } else if (state is InsertLogsLoading || state is UpdateLogsLoading) {
-
-        }
-      },
+    return _provideBlocListener(
       child: FloatingContainer(
           shadowEnabled: false,
           splashEnabled: false,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(widget.log != null ? 'Update your expense' : 'Insert your expense'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(widget.log != null ? 'Update your expense' : 'Insert your expense'),
+                  GestureDetector(
+                    onTap: _onScanTapped,
+                    child: Image.asset("assets/ic_scan.png", height: 24, width: 24),
+                  )
+                ],
+              ),
               const SizedBox(height: 16,),
               TextField(
                 controller: _controllerNominal,
@@ -146,18 +129,56 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
               const SizedBox(height: 8,),
               const Text('Fund Source'),
               _buildListFund(),
-             _buildDateSelectionWidget(),
+              _buildDateSelectionWidget(),
               Align(
-                alignment: Alignment.centerRight,
-                child: ButtonWidget(
-                  controller: _buttonController,
-                  onPressed: () async => _saveExpenseData(),
-                  text: 'Save',
-                )
+                  alignment: Alignment.centerRight,
+                  child: ButtonWidget(
+                    controller: _buttonController,
+                    onPressed: () async => _saveExpenseData(),
+                    text: 'Save',
+                  )
               )
             ],
           )
       ),
+    );
+  }
+
+  Widget _provideBlocListener({required Widget child}) {
+    return BlocListener<LogsBloc, LogsState>(
+        listenWhen: (bloc, state) =>
+        state is InsertLogsResult ||
+            state is InsertLogsLoading ||
+            state is UpdateLogsResult ||
+            state is UpdateLogsLoading,
+        listener: (bloc, state) {
+          if (state is InsertLogsResult || state is UpdateLogsResult) {
+            // After Success Action
+            doAfterSuccess () {
+              // Then update Recent Log List data and others...
+              _logsBloc.add(GetRecentLogsEvent());
+              _balanceLeftBloc.add(GetBalanceLeftEvent());
+              _expenseMonthBloc.add(GetExpenseInMonthEvent());
+
+              // Clear Edit Text
+              _controllerNominal.text = '';
+              _controllerDesc.text = '';
+
+              _buttonController.hideLoading();
+              setState(() {});
+            }
+            if (state is InsertLogsResult && state.isSuccess) {
+              doAfterSuccess();
+            }
+            if (state is UpdateLogsResult && state.isSuccess) {
+              doAfterSuccess();
+              Navigator.pop(context);
+            }
+          } else if (state is InsertLogsLoading || state is UpdateLogsLoading) {
+
+          }
+        },
+        child: child
     );
   }
 
@@ -239,7 +260,7 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
     );
   }
 
-  bool isInputDataValid(){
+  bool isInputDataValid() {
     bool isValid = true;
 
     if(_controllerNominal.text.isEmpty){
@@ -265,8 +286,6 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
 
   LogModel _buildData(){
     String nonDecimalNominal = _controllerNominal.text.replaceAll('.', '');
-    debugPrint("HERE ${_selectedFundSource!.name}");
-    debugPrint("HERE ${_selectedFundSource!.id}");
     return LogModel(
       id: widget.log != null ? widget.log!.id : -1,
       userId: 1,
@@ -294,7 +313,7 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
 
   void _saveExpenseData() async {
     FocusManager.instance.primaryFocus?.unfocus();
-    if(isInputDataValid()){
+    if(isInputDataValid()) {
       _buttonController.showLoading();
       setState(() {});
 
@@ -305,18 +324,47 @@ class _InputExpenseSectionState extends State<InputExpenseSection> {
         // Update data...
         _logsBloc.add(UpdateLogEvent(log: _buildData()));
       }
-
-    }else{
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Some fields are required'), backgroundColor: MyTheme.red,));
     }
   }
 
+  Future<void> _checkPermissionAndInitialize() async {
+    var statusCamera = await Permission.camera.status;
+    var statusStorage = await Permission.storage.status;
+
+    if (statusCamera != PermissionStatus.granted) {
+      final permissionStatus = await Permission.camera.request();
+      if (!permissionStatus.isGranted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Akses kamera dibutuhkan bro")));
+      }
+    }
+
+    if (statusStorage != PermissionStatus.granted) {
+      final permissionStatus = await Permission.storage.request();
+      if (!permissionStatus.isGranted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Akses storage juga dibutuhkan bro")));
+      }
+    }
+  }
+
+  void _onScanTapped() async {
+    await _checkPermissionAndInitialize();
+    var result = await Navigator.pushNamed(context, route.scanPage);
+    if (result is ImageResult) {
+      var textResult = await _textRecognitionHandler.processByteArray(result.bytes);
+      debugPrint("HERE\n$textResult");
+    }
+  }
+
   void _processNominalText(String text){
-    try{
+    try {
       String nonDecimalNominal = text.replaceAll('.', '');
       _controllerNominal.text = MoneyUtil.getReadableMoney(int.parse(nonDecimalNominal));
       _controllerNominal.selection = TextSelection.fromPosition(TextPosition(offset: _controllerNominal.text.length));
-    }catch(e){
+    } catch(e) {
 
     }
   }
