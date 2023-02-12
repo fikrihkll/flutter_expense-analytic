700f6ba66ee2eae2654fc1ecbe2834fb5b0a8b0c
zackmolt <fkrihkl@gmail.com>
2023-02-12 18:27:39 +0700

    feat: create TextRecognitionHandler.swift

---
+++ ios/Runner/TextRecognitionHandler.swift
@@ -0,0 +1,28 @@
+//
+//  TextRecognitionHandler.swift
+//  Runner
+//
+//  Created by Fikri Haikal on 31/01/23.
+//
+
+import Foundation
+import MLKit
+import CoreGraphics
+
+class TextRecognitionHandler {
+    
+    let textRecognizer = TextRecognizer.textRecognizer()
+    
+    func processImage(byteArray:  [UInt8], resulText: @escaping (String?, Text?) -> Void) {
+        let uiImage = UIImage(data: Data(byteArray))!
+        let image = VisionImage(image: uiImage)
+        textRecognizer.process(image) { result, error in
+            guard error == nil, let result = result else {
+                resulText(error?.localizedDescription, nil)
+              return
+            }
+            resulText(nil, result)
+        }
+    }
+    
+}
