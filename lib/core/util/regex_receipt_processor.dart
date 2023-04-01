a3d8f33ca9e0d5ef4e757f572abd5e136cc75671
zackmolt <fkrihkl@gmail.com>
2023-04-01 12:46:22 +0700

    refactor: change regex receipt processor algorithm

--- lib/core/util/regex_receipt_processor.dart
+++ lib/core/util/regex_receipt_processor.dart
@@ -2,23 +2,37 @@ import 'package:expense_app/features/domain/entities/receipt_result.dart';
 
 class RegexReceiptProcessor {
 
-  List<ReceiptResult> convertTextToReceipt(String text) {
-    List<ReceiptResult> listResult = [];
-    for (String line in text.split("\n")) {
-      RegExp regex = RegExp(r'^.+\s.*\d$', multiLine: false, caseSensitive: false);
-      var result = regex.firstMatch(line)?.group(0);
-      if (result != null) {
-        var data = result.split(" ");
-        if (data.length >= 2) {
-          try {
-            listResult.add(
-                ReceiptResult(name: data.first, nominal: int.parse(data.last).toDouble())
-            );
-          } catch(e) {}
-        }
+  List<ReceiptResult> convertTextToReceipt(String receiptString) {
+    final List<String> lines = receiptString.split('\n');
+    final List<double> prices = <double>[];
+    final List<ReceiptResult> results = <ReceiptResult>[];
+
+    // Step 1: Find prices in the receipt
+    for (final String line in lines) {
+      final RegExp regExp = RegExp(r'\d+(?:\.\d+)?');
+      final Match? match = regExp.firstMatch(line);
+      if (match != null) {
+        prices.add(double.parse(match.group(0)!));
       }
     }
-    return listResult;
+
+    // Step 2: Match prices to item names
+    int i = 0;
+    for (final double price in prices) {
+      while (i < lines.length) {
+        final RegExp regExp = RegExp(r'\d+(?:\.\d+)?');
+        if (regExp.hasMatch(lines[i])) {
+          i++;
+          continue;
+        }
+        results.add(ReceiptResult(name: lines[i], nominal: price));
+        i++;
+        break;
+      }
+    }
+
+    // Step 3: Return the results
+    return results;
   }
 
 }
