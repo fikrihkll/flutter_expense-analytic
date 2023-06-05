5a49ee07597248e8d8e79368b9893f370a760561
zackmolt <fkrihkl@gmail.com>
2023-06-05 10:06:40 +0700

    refactor: change properties

--- lib/features/data/models/budget_model.dart
+++ lib/features/data/models/budget_model.dart
@@ -1,13 +1,20 @@
+
+/// LOCAL TABLE
+/// budgets
 class BudgetModel {
   int id;
   String name;
   String description;
+  String ownerUserId;
+  String currency;
   DateTime dateCreated;
   DateTime dateUpdated;
 
   BudgetModel({
     required this.id,
     required this.name,
+    required this.ownerUserId,
+    required this.currency,
     required this.description,
     required this.dateCreated,
     required this.dateUpdated,
@@ -17,7 +24,9 @@ class BudgetModel {
     return {
       'id': id,
       'name': name,
+      'owner_user_id': ownerUserId,
       'description': description,
+      'currency': currency,
       'date_created': dateCreated.toIso8601String(),
       'date_updated': dateUpdated.toIso8601String(),
     };
@@ -27,6 +36,8 @@ class BudgetModel {
     return BudgetModel(
       id: map['id'],
       name: map['name'],
+      ownerUserId: map['owner_user_id'],
+      currency: map['currency'],
       description: map['description'],
       dateCreated: map['date_created'] != null ? DateTime.parse(map['date_created']) : DateTime.now(),
       dateUpdated: map['date_updated'] != null ? DateTime.parse(map['date_updated']) : DateTime.now(),
